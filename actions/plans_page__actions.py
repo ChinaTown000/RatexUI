1913ca1500b3b0d5229352cd0a0c888ce99fa94b
tony <a.kalinichev@brl.ru>
2025-04-17 02:47:29 +0300

    fix

--- actions/plans_page__actions.py
+++ actions/plans_page__actions.py
@@ -1,4 +1,3 @@
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -11,16 +10,16 @@ class PlansPage:
     def open_page(self):
         self.driver.get(self.url)
 
-    def element_is_present(self, locator, timeout=10):
+    def element_is_present(self, locator, timeout=5):
         return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
     def element_is_visible(self, locator, timeout=5):
         return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
-    def element_is_clickable(self, locator, timeout=10):
+    def element_is_clickable(self, locator, timeout=5):
         return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
-    def url_changed(self, url, timeout=10):
+    def url_changed(self, url, timeout=5):
         return WebDriverWait(self.driver, timeout).until(EC.url_changes(url))
 
     def js_click(self, element):
--- test_plans_page.py
+++ test_plans_page.py
@@ -1,5 +1,3 @@
-import time
-
 from pytest import fixture
 
 from actions.plans_page__actions import PlansPage
@@ -14,7 +12,7 @@ def page(webdriver_fixture):
 def test_h1(page):
     assert 'Take Control Of Your Crypto World With RateX Pro' == page.element_is_visible(locator=PlansPageLocators.H1).text
 
-def test_pricing_default_cards_and_clk_buy_now(page):
+def test_pricing_default_cards_text_and_clk_buy_now(page):
 
     assert 'Limited Time Offer to Save!' == page.element_is_visible(locator=PlansPageLocators.FIRST_CARD_HEADER).text
     assert '$299' == page.element_is_visible(locator=PlansPageLocators.FIRST_CARD_PRICE).text
@@ -25,16 +23,14 @@ def test_pricing_default_cards_and_clk_buy_now(page):
     assert '$49' == page.element_is_visible(locator=PlansPageLocators.SECOND_CARD_PRICE).text.split('\n')[1].strip()
     assert '$299' == page.element_is_visible(locator=PlansPageLocators.SECOND_CARD_OLD_PRICE).text
     assert 'Buy Now' == page.element_is_visible(locator=PlansPageLocators.SECOND_CARD_BTN).text
-
     page.js_click(element=page.element_is_clickable(locator=PlansPageLocators.SECOND_CARD_BTN))
     page.url_changed(url=page.url)
     assert 'https://ratex.ai/auth/login?next=%2Fplans%2F' == page.driver.current_url
 
-def test_pricing_enterprise_card_and_clk_learn_more(page):
+def test_pricing_enterprise_card_text_and_clk_learn_more(page):
 
     assert 'Enterprise' == page.element_is_visible(locator=PlansPageLocators.FOURTH_CARD_HEADER).text
     assert 'Learn More' == page.element_is_visible(locator=PlansPageLocators.FOURTH_CARD_BTN).text
-
     page.js_click(element=page.element_is_clickable(locator=PlansPageLocators.FOURTH_CARD_BTN))
     page.url_changed(url=page.url)
     assert 'https://ratex.ai/white-label/' == page.driver.current_url
